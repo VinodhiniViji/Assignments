cff6620e9708fe3619128e6b4ec43c6991e95d7a
VinodhiniViji <92032288+VinodhiniViji@users.noreply.github.com>
2021-10-15 14:44:02 +0530

    Add files via upload

---
+++ test/page/wtchWaitAndMove.page.js
@@ -0,0 +1,17 @@
+import page from '../Page/Page'
+class WtchWaitAndMove extends page
+{
+open()
+{
+super.open("https://www.amazon.in");
+}
+get primelink()
+{
+    return $('//a[@href="/prime?ref_=nav_cs_primelink_nonmember_2860122b63884a8688052dc445e46bd9"]');
+}
+get joinlink()
+{
+  return $('//img[@id="multiasins-img-link"]');
+}
+}
+export default new WtchWaitAndMove();
